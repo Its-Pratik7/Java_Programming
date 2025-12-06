6fdc01644d3a8f7a62d28feedaf0fd735e835521
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-06 15:20:24 +0530

    Update program3.java

--- program3.java
+++ program3.java
@@ -1,7 +1,10 @@
-class program3
+// Class definition
+class Program3
 {
+    // Main method - entry point of the program
     public static void main(String A[])
     {
+        // Print message to the console
         System.out.println("jay ganesh...");
     }
 }
